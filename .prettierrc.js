67d05e57088064725dfdeacd3a8bf85a40fc0a9a
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-05-19 21:43:16 +0200

    chore(editor): set up editor environment and formatting tools

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
