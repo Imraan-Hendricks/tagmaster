21e9a50e7aa75094c4dc75e62a84c50264a47e23
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-05-20 03:39:15 +0200

    build(rollup): configure Rollup for bundling the project

---
+++ rollup.config.mjs
@@ -0,0 +1,38 @@
+import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts';
+import deletePlugin from 'rollup-plugin-delete';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { babel } from '@rollup/plugin-babel';
+
+import pkg from './package.json' assert { type: 'json' };
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
+      deletePlugin({ targets: 'dist/*' }),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+  },
+];
