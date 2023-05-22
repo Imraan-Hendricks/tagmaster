bb11d96727ce6d29049a03e64d74a4de44747d3c
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-05-22 17:58:51 +0200

    feat(select): create select utility for user selection

---
+++ src/prompts/prompt-select.spec.ts
@@ -0,0 +1,81 @@
+import { promptSelect } from './prompt-select';
+import { prompt } from './prompt';
+
+jest.mock('./prompt');
+
+describe('promptSelect', () => {
+  let consoleSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    consoleSpy = jest.spyOn(console, 'log');
+    consoleSpy.mockImplementation(() => {
+      // do nothing
+    });
+  });
+
+  afterEach(() => {
+    consoleSpy.mockRestore();
+  });
+
+  it('should return the selected action', async () => {
+    const mockInstruction = 'Select an action:';
+    const mockOptions = [
+      { ref: '1', action: 'commit', description: 'Make a commit' },
+      { ref: '2', action: 'release', description: 'Perform a release' },
+      { ref: '3', action: 'license', description: 'Generate a license' },
+    ];
+
+    const mockPrompt = 'Enter your choice: ';
+    const mockSelectedOption = '2';
+
+    (prompt as jest.Mock).mockResolvedValueOnce(mockSelectedOption);
+
+    const result = await promptSelect(mockInstruction, mockOptions);
+
+    expect(consoleSpy).toHaveBeenCalledWith(mockInstruction);
+
+    mockOptions.forEach(({ ref, action, description }) => {
+      expect(consoleSpy).toHaveBeenCalledWith(
+        `${ref}) ${action}: ${description}`,
+      );
+    });
+
+    expect(prompt).toHaveBeenCalledWith(mockPrompt);
+    expect(prompt).toHaveBeenCalledTimes(1);
+
+    expect(result).toEqual('release');
+  });
+
+  it('should reprompt when an invalid option is selected', async () => {
+    const mockInstruction = 'Select an action:';
+    const mockOptions = [
+      { ref: '1', action: 'commit', description: 'Make a commit' },
+      { ref: '2', action: 'release', description: 'Perform a release' },
+      { ref: '3', action: 'license', description: 'Generate a license' },
+    ];
+
+    const mockPrompt = 'Enter your choice: ';
+    const mockInvalidOption = '5';
+    const mockValidOption = '2';
+
+    (prompt as jest.Mock)
+      .mockResolvedValueOnce(mockInvalidOption)
+      .mockResolvedValueOnce(mockValidOption);
+
+    const result = await promptSelect(mockInstruction, mockOptions);
+
+    expect(consoleSpy).toHaveBeenCalledWith(mockInstruction);
+
+    mockOptions.forEach(({ ref, action, description }) => {
+      expect(consoleSpy).toHaveBeenCalledWith(
+        `${ref}) ${action}: ${description}`,
+      );
+    });
+
+    expect(prompt).toHaveBeenCalledWith(mockPrompt);
+    expect(prompt).toHaveBeenCalledTimes(2);
+
+    expect(result).toEqual('release');
+  });
+});
---
+++ src/prompts/prompt-select.ts
@@ -0,0 +1,31 @@
+import { prompt } from './prompt';
+
+interface Option<T> {
+  ref: string;
+  action: T;
+  description: string;
+}
+
+export async function promptSelect<T extends string>(
+  instruction: string,
+  options: Option<T>[],
+) {
+  console.log(instruction);
+  options.forEach(({ ref, action, description }) =>
+    console.log(`${ref}) ` + `${action}: ${description}`),
+  );
+
+  let option = await prompt('Enter your choice: ');
+  let action: T | undefined;
+
+  while (!action) {
+    const filtered = options.filter(({ ref }) => ref === option);
+    if (filtered.length === 1) {
+      action = filtered[0].action;
+      break;
+    }
+    option = await prompt('Please enter a valid number as your choice: ');
+  }
+
+  return action;
+}
