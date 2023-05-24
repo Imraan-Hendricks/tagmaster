6e3802d4b9e9428b14677ef41120ed886759cbae
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-05-25 00:52:56 +0200

    feat(commit): create commit module integrating built commit features

---
+++ src/commit/commit.spec.ts
@@ -0,0 +1,93 @@
+import { commit } from './commit';
+import { selectCommitType } from './select-commit-type/select-commit-type';
+import { readScope } from './read-scope/read-scope';
+import { readShortDescription } from './read-short-description/read-short-description';
+import { readLongDescription } from './read-long-description/read-long-description';
+import { selectBreakingChange } from './select-breaking-change/select-breaking-change';
+import { readBreakingChangeDescription } from './read-breaking-change-description/read-breaking-change-description';
+import { selectOpenIssue } from './select-open-issue/select-open-issue';
+import { readOpenIssueReference } from './read-open-issue-reference/read-open-issue-reference';
+import { commitSummary } from './commit-summary/commit-summary';
+import { selectCommitConfirmation } from './select-commit-confirmation/select-commit-confirmation';
+
+jest.mock('./select-commit-type/select-commit-type');
+jest.mock('./read-scope/read-scope');
+jest.mock('./read-short-description/read-short-description');
+jest.mock('./read-long-description/read-long-description');
+jest.mock('./select-breaking-change/select-breaking-change');
+jest.mock(
+  './read-breaking-change-description/read-breaking-change-description',
+);
+jest.mock('./select-open-issue/select-open-issue');
+jest.mock('./read-open-issue-reference/read-open-issue-reference');
+jest.mock('./commit-summary/commit-summary');
+jest.mock('./select-commit-confirmation/select-commit-confirmation');
+
+const mockSelectCommitType = selectCommitType as jest.Mock;
+const mockReadScope = readScope as jest.Mock;
+const mockReadShortDescription = readShortDescription as jest.Mock;
+const mockReadLongDescription = readLongDescription as jest.Mock;
+const mockSelectBreakingChange = selectBreakingChange as jest.Mock;
+const mockReadBreakingChangeDescription =
+  readBreakingChangeDescription as jest.Mock;
+const mockSelectOpenIssue = selectOpenIssue as jest.Mock;
+const mockReadOpenIssueReference = readOpenIssueReference as jest.Mock;
+const mockCommitSummary = commitSummary as jest.Mock;
+const mockSelectCommitConfirmation = selectCommitConfirmation as jest.Mock;
+
+describe('commit', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should perform the commit process', async () => {
+    const mockType = 'feat';
+    const mockScope = 'component';
+    const mockShortDescription = 'Implement new feature';
+    const mockLongDescription =
+      'This feature enhances the component functionality';
+    const mockBreakingChange = 'no';
+    const mockBreakingChangeDescription = undefined;
+    const mockOpenIssue = 'yes';
+    const mockOpenIssueReference = '#123';
+    const mockCommitConfirmation = 'yes';
+
+    mockSelectCommitType.mockResolvedValue(mockType);
+    mockReadScope.mockResolvedValue(mockScope);
+    mockReadShortDescription.mockResolvedValue(mockShortDescription);
+    mockReadLongDescription.mockResolvedValue(mockLongDescription);
+    mockSelectBreakingChange.mockResolvedValue(mockBreakingChange);
+    mockReadBreakingChangeDescription.mockResolvedValue(
+      mockBreakingChangeDescription,
+    );
+    mockSelectOpenIssue.mockResolvedValue(mockOpenIssue);
+    mockReadOpenIssueReference.mockResolvedValue(mockOpenIssueReference);
+    mockSelectCommitConfirmation.mockResolvedValue(mockCommitConfirmation);
+
+    await commit();
+
+    expect(mockSelectCommitType).toHaveBeenCalledTimes(1);
+    expect(mockReadScope).toHaveBeenCalledTimes(1);
+    expect(mockReadShortDescription).toHaveBeenCalledTimes(1);
+    expect(mockReadLongDescription).toHaveBeenCalledTimes(1);
+    expect(mockSelectBreakingChange).toHaveBeenCalledTimes(1);
+    expect(mockReadBreakingChangeDescription).toHaveBeenCalledTimes(0);
+    expect(mockSelectOpenIssue).toHaveBeenCalledTimes(1);
+    expect(mockReadOpenIssueReference).toHaveBeenCalledTimes(1);
+    expect(mockCommitSummary).toHaveBeenCalledWith({
+      type: mockType,
+      scope: mockScope,
+      shortDescription: mockShortDescription,
+      longDescription: mockLongDescription,
+      breakingChange: mockBreakingChange,
+      breakingChangeDescription: mockBreakingChangeDescription,
+      openIssue: mockOpenIssue,
+      openIssueReference: mockOpenIssueReference,
+    });
+    expect(mockSelectCommitConfirmation).toHaveBeenCalledTimes(1);
+  });
+});
--- src/commit/commit.ts
+++ src/commit/commit.ts
@@ -1,3 +1,56 @@
+import { commitSummary } from './commit-summary/commit-summary';
+import { readShortDescription } from './read-short-description/read-short-description';
+import { selectBreakingChange } from './select-breaking-change/select-breaking-change';
+import { selectCommitType } from './select-commit-type/select-commit-type';
+import { selectOpenIssue } from './select-open-issue/select-open-issue';
+import { selectCommitConfirmation } from './select-commit-confirmation/select-commit-confirmation';
+import { readOpenIssueReference } from './read-open-issue-reference/read-open-issue-reference';
+import { readBreakingChangeDescription } from './read-breaking-change-description/read-breaking-change-description';
+import { readLongDescription } from './read-long-description/read-long-description';
+import { readScope } from './read-scope/read-scope';
+
 export async function commit() {
-  return;
+  const type = await selectCommitType();
+  console.log(type + '\n');
+
+  const scope = await readScope();
+  console.log(scope ? '' : 'Skipped!\n');
+
+  const shortDescription = await readShortDescription();
+  console.log();
+
+  const longDescription = await readLongDescription();
+  console.log(longDescription ? '' : 'Skipped!\n');
+
+  const breakingChange = await selectBreakingChange();
+  console.log(breakingChange + '\n');
+
+  let breakingChangeDescription: string | undefined;
+  if (breakingChange === 'yes') {
+    breakingChangeDescription = await readBreakingChangeDescription();
+    console.log();
+  }
+
+  const openIssue = await selectOpenIssue();
+  console.log(openIssue + '\n');
+
+  let openIssueReference: string | undefined;
+  if (openIssue === 'yes') {
+    openIssueReference = await readOpenIssueReference();
+    console.log();
+  }
+
+  commitSummary({
+    type,
+    scope,
+    shortDescription,
+    longDescription,
+    breakingChange,
+    breakingChangeDescription,
+    openIssue,
+    openIssueReference,
+  });
+
+  const commitConfirmation = await selectCommitConfirmation();
+  console.log(commitConfirmation + '\n');
 }
